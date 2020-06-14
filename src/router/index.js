d19b22b48a7cc3fbca61934f11276dd7d1f355c6
Benas Apulskis <benas@ito.lt>
2020-06-14 23:42:01 +0300

    Master event app

---
+++ src/router/index.js
@@ -0,0 +1,33 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import EventList from '../views/EventList'
+import EventCreate from '../views/EventCreate'
+import EventShow from '../views/EventShow'
+
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/',
+    name: 'event-list',
+    component: EventList
+  },
+  {
+    path: '/event-create',
+    name: 'event-create',
+    component: EventCreate
+  },
+  {
+    path: '/event/:id',
+    name: 'event-show',
+    component: EventShow,
+    props: true
+  }
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  routes
+})
+
+export default router
